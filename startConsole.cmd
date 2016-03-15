2fc6c2e3fac464bfbf0a7399df7480ee470eca86
kalyantadi <kalyan.4dwin@gmail.com>
2016-03-15 12:19:42 +0530

    adding agent

---
+++ startConsole.cmd
@@ -0,0 +1,5 @@
+set GRINDERPATH=C:\Users\tkalyan\Downloads\grinder-3.11-binary\grinder-3.11
+set GRINDERPROPERTIES=grinder.properties
+set CLASSPATH=%GRINDERPATH%\lib\grinder.jar;%CLASSPATH%
+
+java -classpath %CLASSPATH% net.grinder.Console
